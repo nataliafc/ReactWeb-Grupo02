649aca64e7d7a358cf27224d192b6ce4ee7c1a72
felipedefaveri <felipefaver27@gmail.com>
2021-11-24 21:15:10 -0300

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from 'react'
 
 function Home() {
-    return (
-        <div >
-            Homepage
-        </div>
-    );
+    return <div></div>
+        
+    
 }
 
 export default Home;
