54687b4503a20935204ef89960148afa04cd1af2
felipedefaveri <felipefaver27@gmail.com>
2021-11-25 21:33:00 -0300

    Update api.js

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://ecommerce-residencia.herokuapp.com/'
+    baseURL: 'https://ecommerce-residencia.herokuapp.com'
 })
 
 export default api;
